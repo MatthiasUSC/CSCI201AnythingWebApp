88bf2a4114690431feb6d0e923866d4650ecc1f7
Matthias <matthiashorgen@gmail.com>
2021-04-18 18:30:31 -0600

    New stuff

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import data.LoginInfo;
+import deprecated.LoginInfo;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
