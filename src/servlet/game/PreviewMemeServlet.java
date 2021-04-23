805c1fb646e2919a1576410fc614b1f30be15696
AzureTriple <colepete@usc.edu>
2021-04-23 12:41:13 -0700

    Progress (tm)
    connected some of the game code stuff to the servlets

--- src/servlet/game/PreviewMemeServlet.java
+++ src/servlet/game/PreviewMemeServlet.java
@@ -14,91 +14,63 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import game.Room;
-import image.ImageHistory;
+import game.Player;
 import image.TextUtil.AlignmentX;
 import image.TextUtil.AlignmentY;
 import sessionAttributes.SessionAttributeKeys;
 
-/*class PreviewMemeResponse {
-    public final int status;
-    public final String base64Img;
-    
-    public PreviewMemeResponse(int status,String base64Img) { this.status = status; this.base64Img = base64Img; }
-}*/
-
 @WebServlet("/PreviewMemeServlet")
 public class PreviewMemeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private static final String IMAGE_ID_PARAM = "imageId";
-    private static final String TEXT_PARAM = "text";
     private static final Encoder B64 = Base64.getEncoder();
     
-    protected void doPost(final HttpServletRequest request,final HttpServletResponse response) throws ServletException,
-                                                                                               IOException {
-        int status;
-        String base64Img = null;
+    private static interface getter<T> {T get(final String r);}
+    private static enum EditArg {
+        alignmentY(r -> AlignmentY.valueOf(r)),
+        alignmentX(r -> AlignmentX.valueOf(r)),
+        size(r -> Integer.parseInt(r)),
+        outlineSize(r -> Double.parseDouble(r)),
+        fillColor(r -> Color.getColor(r)),
+        outlineColor(r -> Color.getColor(r)),
+        headerBGColor(r -> Color.getColor(r)),
+        footerBGColor(r -> Color.getColor(r)),
+        font(r -> resources.Registry.FONTS.getOrDefault(r,resources.Registry.IMPACT_FONT)),
+        fontStyle(r -> resources.Registry.STYLES.getOrDefault(r,Font.PLAIN)),
+        text(r -> r.toCharArray());
+        
+        final getter<?> g;
+        private <T> EditArg(final getter<T> g) {this.g = g;}
+        @SuppressWarnings("unchecked")
+        private <T> T get(final HttpServletRequest r) {return (T)g.get(r.getParameter(toString()));}
+    }
+    
+    protected void doPost(final HttpServletRequest request,
+                          final HttpServletResponse response)
+                          throws ServletException,IOException {
         try {
-            // acquiring resources
-            int imageId = Integer.parseInt(request.getParameter(IMAGE_ID_PARAM));
-            String text = request.getParameter(TEXT_PARAM);
-            
-            HttpSession session = request.getSession();
-            Room room = (Room)session.getAttribute(SessionAttributeKeys.Room.toString());
-            
-            // transforming image
-            // TODO get when round starts and convert to ImageHistory
-            // BufferedImage image = room.getImages()[imageId];
-            
-            /*
-             * BufferedImage result = TextUtil.align(image, image.getWidth(),
-             * image.getHeight(), TextUtil.AlignmentX.CENTER, TextUtil.AlignmentY.MID,
-             * IMPACT_FONT, Color.WHITE, Color.BLACK, Color.DARK_GRAY, 3.5,
-             * text.toCharArray());
-             */
-            
-            // encoding result to base64
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            // ImageIO.write(result, "PNG", out);
-            // TODO get args from the user.
-            ImageIO.write(pushState(
-                null,
-                null,
-                -1,
-                -1,
-                null,
-                null,
-                null,
-                null,
-                null,
-                -1,
-                (ImageHistory)session.getAttribute(SessionAttributeKeys.Image.toString()),
-                null),"PNG",out);
-            // byte[] bytes = out.toByteArray();
-            
-            // base64Img = Base64.getEncoder().encodeToString(bytes);
-            // status = 0;
+            final ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ImageIO.write(pushState(request,(Player)request.getSession().getAttribute(SessionAttributeKeys.Player.toString())),"PNG",out);
             response.getWriter().append("{\"status\":0,\"base64Img\":\"")
                                 .append(B64.encodeToString(out.toByteArray()))
                                 .append("\"}");
-        } catch(Exception e) {
-            e.printStackTrace();
-            //status = 1;
-        }
-        
-        // packaging results
-        //Gson gson = new Gson();
-        //String json = gson.toJson(new PreviewMemeResponse(status,base64Img));
+            return;
+        } catch(final Exception e) {e.printStackTrace();}
         response.getWriter().append("{\"status\":1,\"base64Img\":null}");
     }
-    
-    private static BufferedImage pushState(final AlignmentY y,final AlignmentX x,final int size,
-                                           final double outlineSize,final Color fill,final Color outline,
-                                           final Color bgH,final Color bgF,final Font f,final int style,
-                                           final ImageHistory ih,final char...text) {
-        ih.pushState(y,x,size,outlineSize,fill,outline,bgH,bgF,f,style,text);
-        return ih.getImage();
+    private static BufferedImage pushState(final HttpServletRequest r,final Player p) {
+        return p.updateImage(
+            EditArg.alignmentY.get(r),
+            EditArg.alignmentX.get(r),
+            EditArg.size.get(r),
+            EditArg.outlineSize.get(r),
+            EditArg.fillColor.get(r),
+            EditArg.outlineColor.get(r),
+            EditArg.headerBGColor.get(r),
+            EditArg.footerBGColor.get(r),
+            EditArg.font.get(r),
+            EditArg.fontStyle.get(r),
+            EditArg.text.get(r)
+        );
     }
 }
--- src/servlets/JoinRoomServlet.java
+++ src/servlets/JoinRoomServlet.java
@@ -1,10 +1,12 @@
 package servlets;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Base64.Decoder;
 
-import javax.servlet.ServletContext;
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import contextAttributes.ContextAttributeKeys;
-import contextAttributes.GameManager;
 import game.Player;
 import game.Room;
 import sessionAttributes.SessionAttributeKeys;
@@ -21,39 +21,91 @@ import sessionAttributes.SessionAttributeKeys;
 @WebServlet("/JoinRoomServlet")
 public class JoinRoomServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static final String OP_PARAM = "op";
+    private static enum Operation {create,join}
     private static final String ROOM_CODE_PARAMETER = "room_code"; // subject to change
+    
+    private static enum Err {
+        couldNotJoin,couldNotCreate;
+        
+        private void respond(final HttpServletResponse r) throws IOException {
+            r.getWriter().append(Integer.toString(ordinal()));
+        }
+    }
+    
+    private static final Decoder B64 = Base64.getDecoder();
+    
+    private static interface getter<T> {T get(final String r);}
+    private enum CreateRoomArg {
+        maxPlayers(r -> Byte.parseByte(r)),
+        timeLimit(r -> 1000L * Integer.parseInt(r)),
+        rounds(r -> Integer.parseInt(r)),
+        images(r -> {
+            final String[] b64 = r.split(",");
+            final BufferedImage[] i = new BufferedImage[b64.length];
+            for(int t = 0;t < b64.length;++t) {
+                try {i[t] = ImageIO.read(new ByteArrayInputStream(B64.decode(b64[t])));}
+                catch(final IOException e) {}
+            }
+            return i;
+        });
+        
+        final getter<?> g;
+        private <T> CreateRoomArg(final getter<T> g) {this.g = g;}
+        @SuppressWarnings("unchecked")
+        private <T> T get(final HttpServletRequest r) {return (T)g.get(r.getParameter(toString()));}
+    }
     
     protected void doPost(final HttpServletRequest request,
                           final HttpServletResponse response)
                           throws ServletException,IOException {
         try {
-            // recieve room params from client
-            Integer roomCode = Integer.parseInt(request.getParameter(ROOM_CODE_PARAMETER));
-            
-            HttpSession session = request.getSession();
-            ServletContext context = request.getServletContext();
-            GameManager gameManager = (GameManager)context.getAttribute(ContextAttributeKeys.GameManager.toString());
-            
-            // TODO check if already in a room? or playing a game? how to handle, just leave
-            // other game, or throw error?
-            
-            // try find room and join
-            Room room = gameManager.getGame(roomCode);
-            Player player = room.addPlayer();
-            
+            final Room room = switch(Operation.valueOf(request.getParameter(OP_PARAM))) {
+                case create -> Room.createRoom(
+                    CreateRoomArg.maxPlayers.get(request),
+                    CreateRoomArg.timeLimit.get(request),
+                    CreateRoomArg.rounds.get(request),
+                    CreateRoomArg.images.get(request)
+                );
+                case join -> Room.getLobby(Byte.parseByte(request.getParameter(ROOM_CODE_PARAMETER)));
+            };
+            if(room == null) {Err.couldNotJoin.respond(response); return;}
+            final Player player = room.addPlayer();
             // link room and player to session
+            final HttpSession session = request.getSession();
             session.setAttribute(SessionAttributeKeys.Room.toString(),room);
             session.setAttribute(SessionAttributeKeys.Player.toString(),player);
-            
-            response.getWriter().append("success");
-        } catch(InterruptedException|NumberFormatException|
-                GameManager.GameNotFoundException|NullPointerException e) {
+            //TODO send info to clients
+        } catch(InterruptedException|NumberFormatException|NullPointerException e) {
             e.printStackTrace();
-            response.getWriter().append(e.getMessage());
-        } catch(Exception e) { //TODO do we need this?
+            Err.couldNotCreate.respond(response);
+        } catch(final Exception e) {
             e.printStackTrace();
-            response.getWriter().append(e.getMessage());
+            Err.couldNotCreate.respond(response);
         }
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
